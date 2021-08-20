6d7132c81f5913fb3452a96200c5ae8aaefee609
Gavin <gavinbrrtt@gmail.com>
2021-08-20 12:47:36 -0700

    Added argparse module as well as full and evasive scanning modes. Added a README with basic project information.

---
+++ README.md
@@ -0,0 +1,2 @@
+## Description
+This repo contains a small threaded TCP port scanner written in Python. The scanner allows for deep port scanning and very basic IDS detection.
--- scanner.py
+++ scanner.py
@@ -1,37 +1,51 @@
 import re
-import sys
 import socket
+from time import sleep
 from queue import Queue
+from random import sample
 from threading import Thread, Lock
+from argparse import ArgumentParser
 
 print_lock = Lock()
 worker_queue = Queue()
 
+def get_port_list(full_scan, evasive_scan):
+	if full_scan:
+		ports = list(range(1, 65535))
+		if evasive_scan: return sample(ports, 65534)
+	if evasive_scan: return sample(list(range(1, 9999)), 9998)
+	return list(range(1, 9999))
+
 def scan(target):
+	# create a socket object to test the connection
 	tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	try:
-		conn = tcp_socket.connect_ex(target)
+		# attempt to connect to the port
+		conn = tcp_socket.connect_ex(target[0])
 		with print_lock:
+			# check for open tcp port
 			if conn == 0:
-				print(f'{target[1]} -> open')
+				print(f'{target[0][1]} -> open')
 	except:
 		with print_lock:
 			print(f'Error connecting to target')
 	finally:
+		# close the socket
 		tcp_socket.close()
+		# sleep for 1 second to help evade IDS detection
+		if target[1]: sleep(1)
 
-def port_scan(address):
-	print(f'Scanning {address}')
+def port_scan(address, full, evasive):
 	# generate a list of ports to scan
-	ports = list(range(10, 10000))
-	#ports = [22, 25, 53, 143, 110, 80, 443, 8080, 9100]
+	ports = get_port_list(full, evasive)
+	print(f'Scanning {address}')
 	for t in range(10):
 		scanner = Thread(target=scan_threader)
 		scanner.daemon = True
 		scanner.start()
 	# put the targets into the queue
 	for port in ports:
-		worker_queue.put((address, port))
+		worker_queue.put(((address, port), evasive))
 	# wait for threads to finish
 	worker_queue.join()
 
@@ -41,11 +55,18 @@ def scan_threader():
 		scan(work)
 		worker_queue.task_done()
 
+def parse():
+	parser = ArgumentParser()
+	parser.add_argument("-f", "--full", action="store_true")
+	parser.add_argument("-e", "--evasive", action="store_true")
+	parser.add_argument("address", action="store", help="Please add an IPv4 address")
+	return parser.parse_args()
+
 if __name__ == "__main__":
 	try:
-		args = sys.argv
-		if len(args) != 2 or not re.match(r'^[0-9]{1,3}(\.[0-9]{1,3}){3}$', args[1]): raise ValueError('Please enter an IPv4 address.')
+		parsed = parse()
+		if not parsed.address or not re.match(r'^[0-9]{1,3}(\.[0-9]{1,3}){3}$', parsed.address): raise ValueError('Please enter an IPv4 address.')
 		
-		port_scan(args[1])
-	except:
-		print('error')
+		port_scan(parsed.address, parsed.full, parsed.evasive)
+	except Exception as error:
+		print(error)
